529545e3752ba4ff7e2247854ce8949d69a4f94f
lluana20 <luanapereiralima186@gmail.com>
2023-08-05 19:38:23 -0300

    questao 3

---
+++ questao3/questao3.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Questão 3</title>
+  <style>
+    .titulo{
+      color: white;
+      background-color: black;
+    }
+    .titulo-footer{
+      background-color: gray;
+    }
+    
+
+  </style>
+</head>
+
+<body>
+  <header>
+    <h2 class="titulo-footer">Desenvolvimento Web</h2>
+  </header>
+  <main>
+    <h2 class="titulo">Trabalho: Questão 3</h2>
+   
+        <p>Pergunta 1: Qual a função em PHP usada para exibir informações sobre uma variável ou expressão de uma forma legível para humanos?</p>
+
+        <ol type="A">
+          <li><a href="form2.php?q1=letraA" style="color:green">print_r</a></li>
+          <li><a href="form2.php?q1=letraB">echo</a></li> 
+          <li><a href="form2.php?q1=letraC">var_dump</a></li> 
+          <li><a href="form2.php?q1=letraD">print</a></li> 
+          <li><a href="form2.php?q1=letraE">printf</a></li> 
+        </ol>
+        <br>
+        <p><a href="index,php">Página Inicial</a></p>
+
+  </main>
+  <footer > Luana Lima & Maria Theresa &copy; IFNMG 2023 </footer>
+</body>
+
+</html>
